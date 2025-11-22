ada121a9e7317d7fd6d4b76be557729648840dc0
lluan-iada <luan.nascimento@sptech.school>
2025-11-22 09:28:19 -0300

    Adição do campo id na validação da sessão - O ID não estava sendo salvo corretamente, fiz a correção, adicionando o a variavel do id de forma que ela seja armazenada no sessionStorage

--- site-api/public/js/sessao.js
+++ site-api/public/js/sessao.js
@@ -2,10 +2,11 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var id = sessionStorage.ID_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null) {
+    if (email != null && nome != null && id != null) {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
